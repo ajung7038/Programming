1125b372494029bdfa615fb9a88ff7d169f4106d
아정 <80907516+ajung7038@users.noreply.github.com>
2023-05-07 19:32:04 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,44 +1,21 @@
-def push(num):
-    stack.append(num)
-    
-def pop():
-    if not stack:
-        print(-1)
-    else:
-        a=stack.pop()
-        print(a)
-        
-def size():
-    print(len(stack))
-    
-def empty():
-    if not stack:
-        print(1)
-    else:
-        print(0)
-        
-def top():
-    if not stack:
-        print(-1)
-    else:
-        print(stack[-1])
-    
-    
- # 입력
-import sys
-N=int(sys.stdin.readline())
-stack=[]
+# 함수 사용 x
+import sys 
+n = int(sys.stdin.readline())
 
-for repeat in range(N):
-    command="" # command 초기화
-    command=sys.stdin.readline().strip().split()
-    if command[0]=="push":
-        push(command[1]) # push 값 전달
-    elif command[0]=="pop":
-        pop()
-    elif command[0]=="size":
-        size()
-    elif command[0]=="empty":
-        empty()
-    elif command[0]=="top":
-        top()
+stack = []
+
+for _ in range(n):
+    s = sys.stdin.readline().split() 
+    if s[0] == 'push':
+        stack.append(s[1])
+    elif s[0] == 'pop':
+        if len(stack)>0: print(stack.pop())
+        else: print(-1)
+    elif s[0] == 'size':
+        print(len(stack))
+    elif s[0] == 'empty':
+        if len(stack)==0: print(1)
+        else: print(0)
+    elif s[0] == 'top':
+        if len(stack)>0: print(stack[-1])
+        else: print(-1)
