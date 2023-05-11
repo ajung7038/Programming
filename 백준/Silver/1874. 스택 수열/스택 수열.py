e206d1117e16506153dba24ca77a45c9bc856905
아정 <80907516+ajung7038@users.noreply.github.com>
2023-05-11 21:33:08 +0900

    [Silver II] Title: 스택 수열, Time: 4080 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,62 @@
+# 스택 수열
+
+'''
+1. 숫자를 입력받는다(수열)
+2. for i in range(1, n+1):
+3. if i < (1.):
+-> if i not in stack:
+-> append(i) => print('+')
+-> 1-1번으로 돌아가기
+
+4. if i > (1.):
+    break
+5. if i == (1.): stack[-1]?
+    if i in stack[-1]:
+        print('-')
+    else:
+        append => print('+')
+        print('-')
+        stack.pop()
+        ->1-1번으로 돌아감
+        
+# try -> 실패...
+stack=[]
+n=int(input())
+for i in range(1, n+1):
+    seq=int(input())
+    if seq not in stack:
+        if i<=seq:
+            stack.append(i)
+            print('+')
+        else:
+            i-=1
+    if seq in stack:
+        if seq==stack[-1]:
+            stack.pop()
+            print('-')
+            '''
+
+n = int(input())
+stack = []
+answer = []
+flag = 0
+cur = 1
+for i in range(n):
+    num = int(input())
+    while cur <= num:       # 입력한 수를 만날 때 까지 오름차순으로 push
+        stack.append(cur)
+        answer.append("+")
+        cur += 1
+    # 입력한 수를 만나면 while문 탈출. 즉 cur = num일 때 까지 while문을 돌아 스택을 쌓는다.
+
+    if stack[-1] == num:    # stack의 TOP이 입력한 숫자와 같다면
+        stack.pop()         # 스택의 TOP을 꺼내 수열을 만들어 준다.
+        answer.append("-")
+    else:                   # stack의 TOP이 입력한 수가 아니면 주어진 스택을 만들 수 없다.
+        print("NO")         # 왜냐하면 12345 처럼 오름차순으로 스택이 입력되는데
+        flag = 1            # TOP이 num보다 크면 num은 TOP보다 더 아래에 쌓여있기 때문이다.
+        break               
+
+if flag == 0:
+    for i in answer:
+        print(i)
