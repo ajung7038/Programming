f32ff7a15e47452104fc9e1fd45ac9477ff09a31
아정 <80907516+ajung7038@users.noreply.github.com>
2023-09-11 12:54:59 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        String B = sc.next();
+
+        sc.close();
+
+        // 문자를 숫자로 변환 (아스키코드)
+        // ex) '3'-'0' => 3
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
+        System.out.println(A * Integer.parseInt(B));
+    }
+}
